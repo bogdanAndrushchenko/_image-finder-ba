8e5dec19a052bb4136a801e20338bca362b33b61
Bogdan Andrushchenko <bandrushchenko15@gmail.com>
2020-10-13 14:36:56 +0300

    add notification vs pnotiy

--- src/scripts/apiService.js
+++ src/scripts/apiService.js
@@ -1,3 +1,4 @@
+import {error} from './notification'
 export default {
   page:1,
   search: '',
@@ -5,17 +6,25 @@ export default {
   APIkey: '18613871-d09d7f4d1ad86f8a51a1289a6',
   baseURL: "https://pixabay.com/api/?image_type=photo&orientation=horizontal&q",
   async getResourse   ()  {
-
-    const responce = await fetch(`${this.baseURL}=${this.search}&page=${this.page}&per_page=${this.per_page}&key=${this.APIkey}`);
-
-    if (!responce.ok) {
-        throw new Error(`Ошибка по ${this.search} статус${responce.status}`)
-    }
-    this.page +=1;
-    return await responce.json();
+    try{
+      const responce = await fetch(`${this.baseURL}=${this.search}&page=${this.page}&per_page=${this.per_page}&key=${this.APIkey}`);
+      this.page +=1;
+      
+     
+      return await responce.json();
     
+    
+    }
+    catch(err) {
+      throw error({
+         text:(`Ошибка по ${this.search} статус${responce.status}`),
+         hide:true,
+         delay:2000,
+         width: "200px",
+       });
+      
+    }
   },
-
   resetPage(){
   this.page = 1;
   },
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,29 +1,53 @@
-import apiService1 from './apiService';
+import apiService from './apiService';
+import {error, alert} from './notification'
 import {
   updateLi_item_Markup,
   cleanMarkupBefore,
   scrollPage,
 } from './updateMarkup';
 import refs from './refs';
+import { data } from 'autoprefixer';
 
 
 refs.searcForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const form = event.currentTarget;
-  apiService1.search = form.elements.query.value;
+  apiService.search = form.elements.query.value;
   cleanMarkupBefore();
   form.reset();
 
-  apiService1.resetPage();
-  apiService1.getResourse().then(data => updateLi_item_Markup(data))
-  console.log(apiService1.search);
+  apiService.resetPage();
+  apiService.getResourse().then(data => {
+    const mach = data.hits.length 
+    if(mach === 0){
+      error(`На Ваш запрос
+      ${apiService.search}
+      найдено ${mach} 
+      картинок`)
+      return
+    }
+
+    alert({
+      text:(`Ваш запрос ${apiService.search}  
+      обработан успешно`),
+      hide:true,
+      delay:1500,
+    });
+    updateLi_item_Markup(data)
+    
+  })
 });
 
 refs.loadMore.addEventListener('click', () => {
-  apiService1.getResourse().then(data => {
+  apiService.getResourse().then(data => {
+    alert({
+      text:(`Ваш запрос ${apiService.search}  
+      обработан успешно`),
+      hide:true,
+      delay:1500,
+    });
     updateLi_item_Markup(data);
     scrollPage();
-    console.log(apiService1.search, apiService1.page);
   })
 })
